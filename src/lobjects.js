985a02f160fb65a5cd4de40feb9565517575357c
WilliamLewww <WilliamLew32@gmail.com>
2017-01-10 23:32:55 -0800

    changed intro timing

--- src/lobjects.js
+++ src/lobjects.js
@@ -6,8 +6,33 @@ class FadeObject {
     this.rectangle = new Rectangle(new Vector2(0,0), new Vector2(SCREENWIDTH, SCREENHEIGHT), color, initialState);
   }
 
+  SetSpeed(speed) {
+    this.speed = speed;
+  }
+
   Run() {
     if (this.visible == true) {
+      if (this.rectangle.alpha - this.speed <= 0) {
+        this.rectangle.alpha = 0;
+        this.visible = false;
+      }
+      else {
+        this.rectangle.alpha -= this.speed;
+      }
+    }
+    else {
+      if (this.rectangle.alpha + this.speed >= 1) {
+        this.rectangle.alpha = 1;
+        this.visible = true;
+      }
+      else {
+        this.rectangle.alpha += this.speed;
+      }
+    }
+  }
+
+  RunOnce(state) {
+    if (state == true) {
       if (this.rectangle.alpha - this.speed <= 0) {
         this.rectangle.alpha = 0;
         this.visible = false;
--- src/main.js
+++ src/main.js
@@ -1,48 +1,36 @@
-var renderer = PIXI.autoDetectRenderer(SCREENWIDTH, SCREENHEIGHT,{backgroundColor : 0x000000});
+var renderer = PIXI.autoDetectRenderer(SCREENWIDTH, SCREENHEIGHT,{backgroundColor : 0x220000});
 document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
 
 var text = new PIXI.Text("William's Showcase", {font:"75px Times New Roman", fill:"darkred"});
-text.x = (SCREENWIDTH / 2) - (text.width / 2);
-text.y = (SCREENHEIGHT / 2) - (text.height / 2);
+text.x = (SCREENWIDTH / 2) - (text.width / 2); text.y = (SCREENHEIGHT / 2) - (text.height / 2);
+var text2 = new PIXI.Text("of useless shit", {font:"25 Times New Roman", fill:"#090909"});
+text2.x = (SCREENWIDTH / 2) - (text2.width / 2); text2.y = (SCREENHEIGHT / 2) + text2.height;
+text2.visible = false;
 stage.addChild(text);
+stage.addChild(text2);
 
 var graphics = new PIXI.Graphics();
 stage.addChild(graphics);
 
-var fade = new FadeObject(true, .01, 0x000000);
+var fade = new FadeObject(true, .02, 0x000000);
 var exitIntro = false, createText = false;
 
-function RunIntro() {
-  if (exitIntro == false) {
-    if (fade.visible == true) {
-      if (createText == true) {
-        exitIntro = true;
-      }
-      fade.Run();
-    }
-    else {
-      setTimeout(function() {
-        var text2 = new PIXI.Text("of useless shit", {font:"25 Times New Roman", fill:"#090909"});
-        text2.x = (SCREENWIDTH / 2) - (text2.width / 2);
-        text2.y = (SCREENHEIGHT / 2) + text2.height;
-        stage.addChildAt(text2, 0);
-        createText = true;
-        setTimeout(function() {
-        if (fade.visible == false) {
-          fade.Run();
-        } }, 500);
-      }, 350);
-    }
-  }
-}
+var runTransition = false;
 
 Update();
 function Update() {
     requestAnimationFrame(Update);
-    setTimeout(function() { RunIntro(); }, 1000);
-
+    if (exitIntro == false) { setTimeout(function() { RunIntro(); }, 1000); }
+    if (runTransition == true) {
+      RunTransition();
+    }
     Draw();
+}
+
+function RunTransition() {
+  fade.SetSpeed(.005);
+  fade.RunOnce(true);
 }
 
 function Draw() {
@@ -50,4 +38,28 @@ function Draw() {
 
   renderer.render(stage);
   graphics.clear();
+}
+
+function RunIntro() {
+  if (exitIntro == false) {
+    if (fade.visible == true) {
+      if (createText == true) {
+        stage.removeChildren(0,2);
+        runTransition = true;
+        exitIntro = true;
+      }
+      fade.Run();
+    }
+    else {
+      setTimeout(function() {
+        fade.SetSpeed(.01);
+        text2.visible = true;
+        createText = true;
+        setTimeout(function() {
+        if (fade.visible == false) {
+          fade.Run();
+        } }, 50);
+      }, 500);
+    }
+  }
 }
